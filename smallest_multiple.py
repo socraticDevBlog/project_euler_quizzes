5394cdf6244cad8b617406be99b0ccee4796de23
socraticDevBlog <maximebonin@outlook.com>
2019-12-14 19:41:21 -0500

    smallest divisible by one to twenty without leftover

---
+++ smallest_multiple.py
@@ -0,0 +1,24 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+dividers = range(1, 21)
+
+smallestNumberFound = False
+
+i = 0
+number = 20
+
+while(not smallestNumberFound):
+
+    if (number % dividers[i] != 0):
+        i = 0
+        number += 1
+        continue
+    else:
+        i += 1
+
+    if (i == 20):
+        smallestNumberFound = True
+
+print(number)
