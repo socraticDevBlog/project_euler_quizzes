7ff27f4d76edcddb0e84547511607a4f80d9f64e
socraticDevBlog <maximebonin@outlook.com>
2019-12-14 19:51:29 -0500

    minor correction

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -2,15 +2,12 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-dividers = range(1, 21)
-
 smallestNumberFound = False
-
-i = 0
 number = 20
+dividers = range(1, 21)
+i = 0
 
 while(not smallestNumberFound):
-
     if (number % dividers[i] != 0):
         i = 0
         number += 1
